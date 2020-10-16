d54c7cc1ba9805d108700559c0fdab7823dfb8c0
Christian Lewis <christianlewis024@gmail.com>
2020-10-15 21:34:57 -0700

    email password auth setup and basic routing

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDgF-KT5SuR6y56eqXMyaxgcuzlmsFSQ_U",
+  authDomain: "messenger-46f50.firebaseapp.com",
+  databaseURL: "https://messenger-46f50.firebaseio.com",
+  projectId: "messenger-46f50",
+  storageBucket: "messenger-46f50.appspot.com",
+  messagingSenderId: "740240374279",
+  appId: "1:740240374279:web:004e1922d6a474a1a688a2",
+  measurementId: "G-ZVC07ZX26N"
+});
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth}
